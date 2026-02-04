26f27430b6cab448a97a4020b467fc29e170aea0
Quentin Donnelly <quentindonnelly91@gmail.com>
2026-02-04 14:53:20 -0500

    fix(infra): add explicit empty filters for future compatibility

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -26,6 +26,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
   rule {
     id     = "expire-old-versions"
     status = "Enabled"
+    filter {}
 
     noncurrent_version_expiration {
       noncurrent_days = 30
